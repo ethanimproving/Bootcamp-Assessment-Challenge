da803e5e6ca57ccf11b995a1a491a1646f961f02
Ethan Miller <Ethan.Miller@improving.com>
2019-11-29 20:13:53 -0600

    get purchaser and total amount spent as key and value in json string

--- src/main/java/org/improving/Main.java
+++ src/main/java/org/improving/Main.java
@@ -1,6 +1,11 @@
 package org.improving;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.improving.client.PurchaseClient;
+import org.improving.entity.Purchaser;
+
+import java.util.List;
 
 
 public class Main {
@@ -8,6 +13,24 @@ public class Main {
     public static void main(String[] args) {
         var purchaseClient = new PurchaseClient();
         var purchaseRepository = new PurchaserRepository();
-        System.out.println(purchaseRepository.getTotalAmountSpent());
+        var dataList = purchaseRepository.getTotalAmountSpent();
+        var jsonString = convertObjectsToProperties(dataList);
+        System.out.println(jsonString);
+    }
+
+    public static String convertObjectsToProperties(List<Purchaser> list) {
+        var sb = new StringBuilder();
+        sb.append("{");
+
+        var i = 0;
+        for (var purchaser : list) {
+            sb.append("\n    \"" + purchaser.getPurchaser() + "\": " + purchaser.getTotalAmountSpent());
+            if (i < list.size()-1) sb.append(",");
+            i++;
+        }
+
+        sb.append("\n}");
+
+        return sb.toString();
     }
 }
